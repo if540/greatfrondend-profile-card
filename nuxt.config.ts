ebdd1c010b134bde14010916427064464e31d8fc
Kerwin <if5400@gmail.com>
2024-07-03 15:20:21 +0800

    install dep module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-03"
+  compatibilityDate: "2024-07-03",
+
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/greatfrondend-profile-card/' : '/',
+    buildAssetsDir: '/static/'
+  },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
